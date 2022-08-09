34437ceb3fd8fb749f4e2c7c957bc4bb0821b048
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2022-08-09 13:32:00 +1000

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
 cd
 set /P file="which file would you like to compile? "
-py -m nuitka %file% --standalone --onefile --windows-icon-from-ico=icon.ico --windows-disable-console --enable-plugin=tk-inter
+py -m nuitka %file% --standalone --onefile --windows-icon-from-ico=icon.ico --enable-plugin=tk-inter
 pause
