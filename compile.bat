87bafadc9fdbc4d5c86413542beb46da96ed0c6f
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2022-08-12 13:05:37 +1000

    update

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@ cd
 rem --enable-plugin=numpy
 CHOICE /C mo /M "Press M to compile main.pyw or Press O to compile another file."
 
-IF %ERRORLEVEL% == 1 py -m nuitka main.pyw --standalone --onefile --windows-icon-from-ico=icon.ico --enable-plugin=tk-inter 
+IF %ERRORLEVEL% == 1 py -m nuitka main.pyw --standalone --windows-disable-console --onefile --windows-icon-from-ico=icon.ico --enable-plugin=tk-inter 
 
 IF %ERRORLEVEL% == 2 set /P file="which file would you like to compile? "
 py -m nuitka %file% --standalone --onefile --windows-icon-from-ico=icon.ico --enable-plugin=tk-inter
